3fb1ca98855e0d0c93becd0f2e4f4b9b7c609783
Sergey Shulga <shul.sergio@gmail.com>
2024-09-07 17:00:32 +0300

    less 2

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,10 +1,44 @@
-import { Container, Heading, Section } from 'components';
+import { Container, CountryInfo, Heading, Loader, Section } from 'components';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchCountry } from 'service/countryApi';
 
 const Country = () => {
+
+  const [country, setCountry] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  
+
+  const {countryId} = useParams();
+  console.log('params-', countryId);
+
+  useEffect(() => { 
+
+    const fetchData = async () => {
+      setIsLoading(true);
+      setError(null);
+      try {
+        const data = await fetchCountry(countryId);
+        setCountry(data); 
+
+      } catch (error) {
+        setError(error);
+      }
+      finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchData();
+  },[countryId])
+
   return (
     <Section>
       <Container>
-        <Heading title="SearchCountry" bottom />
+        {country && <CountryInfo {...country} />}
+        {isLoading && <Loader />}
+        {error && <Heading title="Its wrong" bottom />}
       </Container>
     </Section>
   );
